5d433ac6421a49c1f0360b1c0121ff4daa55199d
AdrianAbusland <Adrian.Abusland@gmail.com>
2024-05-23 11:44:52 +0200

    JS
    Started adding the api and html for the feed page

---
+++ js/constants.mjs
@@ -0,0 +1,3 @@
+const API_BASE_URL = 'https://v2.api.noroff.dev';
+
+export const API_BLOGPOST_URL = `${API_BASE_URL}/blog/posts/{name}`;
---
+++ js/feedPage.js
@@ -0,0 +1,10 @@
+import { API_BLOGPOST_URL } from "./constants.mjs";
+import { doFetch } from "./doFetch.mjs";
+
+// Funtion to generate HTML on Blog Feed Page (HomePage)
+function generateFeedHTML (hotViewLabs) {
+
+    // Create container for banner carousel section
+    const bannerCarousel = document.createElement('section');
+    bannerCarousel.classList.add('bannerCarousel')
+}
