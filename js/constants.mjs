d1734e07a587b44c0d20adc510c80efd0827261e
AdrianAbusland <Adrian.Abusland@gmail.com>
2024-05-24 13:55:09 +0200

    update constants.mjs

--- js/constants.mjs
+++ js/constants.mjs
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://v2.api.noroff.dev';
+const API_BASE_URL = 'https://v2.api.noroff.dev/blog/posts/HotViewLab';
 
 // Function to generate the blog post URL
-export const getApiBlogPostUrl = `${API_BASE_URL}/blog/posts/<name>`;
+export const getApiBlogPostUrl = (endpoint) => `${API_BASE_URL}/${endpoint}`;
